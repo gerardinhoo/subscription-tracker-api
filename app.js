0c73365f48ae588e45e69020551860fe40feb7ed
Gerard Eklu <gerard.eklu@yahoo.fr>
2025-03-19 20:15:10 -0400

    Implemented global error handling using a middleware

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/authRouter', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use(errorMiddleware);
 
 
 app.get('/', (req, res) => {
