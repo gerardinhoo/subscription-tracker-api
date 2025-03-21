c52f44808308f170691c7afec0e8c8a0b95d9e9e
Gerard Eklu <gerard.eklu@yahoo.fr>
2025-03-21 01:06:18 -0400

    Implemented the workflow for sending the reminders

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import arcjetMidlleware from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.use(cookieParser());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter)
+
 app.use(errorMiddleware);
 app.use(arcjetMidlleware)
 
